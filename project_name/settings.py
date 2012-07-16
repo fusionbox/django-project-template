10375075fc8ffb2c792a6bdafea274fb72fcfcde
Chris Proto <cproto@fusionbox.com>
2012-07-16 13:22:21 -0600

    add signed cookies as the default engine

--- project_name/settings.py
+++ project_name/settings.py
@@ -168,6 +168,10 @@ COMPRESS_PRECOMPILERS = (
 
 FORCE_SCRIPT_NAME = ''
 
+SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
+# <https://www.owasp.org/index.php/HTTPOnly#Browsers_Supporting_HttpOnly>
+SESSION_COOKIE_HTTPONLY = True
+
 # Debug Toolbar Settings
 INTERNAL_IPS = (
         '127.0.0.1',
