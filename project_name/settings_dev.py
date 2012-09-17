8f558fa3462445f34ce8e633d969b0927792932c
Aaron Merriam <amerriam@fusionbox.com>
2012-09-17 10:36:22 -0600

    changed default project to be postgres

--- project_name/settings_dev.py
+++ project_name/settings_dev.py
@@ -1,6 +1,6 @@
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': '{{ project_name }}',
         'USER': '{{ project_name }}_user',
     }
