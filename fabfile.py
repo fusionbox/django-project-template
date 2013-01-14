11a60e5f360be400f9ef804efaabb50d3e07907f
David Sanders <dsanders@fusionbox.com>
2013-01-14 16:03:24 -0700

    Add fusionbox fabric helpers

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,10 @@
-from fusionbox.fabric_helpers import *
+from fabric.api import env, roles
 
-env.roledefs = {
-        'dev': ['dev.fusionbox.com'],
-        }
+from fusionbox.fabric import fb_env
+from fusionbox.fabric.django import stage, deploy
 
-env.project_name = '{{ project_name }}'
-env.short_name = '{{ project_name }}'
+# env.roledefs['live'] = ['fusionbox@{{ project_name }}.com']
+
+fb_env.project_name = '{{ project_name }}'
 
 stage = roles('dev')(stage)
