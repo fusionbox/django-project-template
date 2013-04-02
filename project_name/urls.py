6358623a01254d5b156cd164e1b9410ece225224
Gavin Wahl <gwahl@fusionbox.com>
2013-04-02 16:45:34 -0600

    use TemplateView instead of the removed direct_to_template

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -6,8 +7,8 @@ admin.autodiscover()
 from django.conf import settings
 
 urlpatterns = patterns('',
-    url('^robots.txt$', 'django.views.generic.simple.direct_to_template', {'template': 'robots.txt', 'mimetype': 'text/plain'}),
-    # url('^sitemap.xml$', 'django.views.generic.simple.direct_to_template', {'template': 'sitemap.xml', 'mimetype': 'application/xml'}),
+    url('^robots.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain'),
+    # url('^sitemap.xml$', TemplateView.as_view(template_name='sitemap.xml', content_type='application/xml'),
 
     # Examples:
     # url(r'^$', '{{ project_name }}.views.home', name='home'),
