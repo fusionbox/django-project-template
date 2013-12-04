42c0f1ddc443a4a381421267411195cf85a77fff
Gavin Wahl <gwahl@fusionbox.com>
2013-12-04 10:45:23 -0700

    configure a cache on dev

--- project_name/settings_dev.py
+++ project_name/settings_dev.py
@@ -26,3 +26,11 @@ RAVEN_CONFIG = {
 TEMPLATE_LOADERS = (
     ('django.template.loaders.cached.Loader', TEMPLATE_LOADERS),
 )
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+        'KEY_PREFIX': '{{ project_name }}',
+    }
+}
