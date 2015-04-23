f6708e51c10006b587b842487a8f0b9439e1a3ea
Antoine Catton <acatton@fusionbox.com>
2015-04-23 13:30:00 -0600

    New deploy way

--- project_name/settings_dev.py
+++ project_name/settings_dev.py
@@ -2,14 +2,6 @@ from settings import *
 
 DEBUG = True
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '{{ project_name }}',
-        'USER': '{{ project_name }}_user',
-    }
-}
-
 DEBUG_TOOLBAR_CONFIG = {
     'INTERCEPT_REDIRECTS': False,
 }
