21eca8d47021669a92bdf06127fcc5055805bf24
Alexander Bliskovsky <abliskovsky@fusionbox.com>
2015-11-23 14:28:29 -0700

    Update fab file to new format

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,24 @@
 from fabric.api import env, roles
 
-from fusionbox.fabric import fb_env
-from fusionbox.fabric.django import stage, deploy
+from fusionbox.fabric.django.new import stage, deploy
 
-# env.roledefs['live'] = ['fusionbox@{{ project_name }}.com']
 
-fb_env.project_name = '{{ project_name }}'
+def dev():
+    env.project_name = '{{ project_name }}.dev'
+    env.vassal_name = '{{ project_name }}_dev'
+
+    return ['fusionbox@{{ project_name }}.dev.fusionbox.com']
+
+
+# def live():
+#     env.project_name = '{{ project_name }}.com'
+#     env.vassal_name = '{{ project_name }}_com'
+
+#     return ['fusionbox@demo.{{ project_name }}.com']
+
+
+env.roledefs['dev'] = dev
+#env.roledefs['live'] = live
 
 stage = roles('dev')(stage)
+#deploy = roles('live')(deploy)
